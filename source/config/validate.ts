a1e63f5b601c946b3c2a81f5ae79325ac00f30a5
chkt <ck@chkt.one>
2020-09-15 15:16:06 +0200

    Add validateStringNonempty(), validateStringLength(), validateStringHex(), validateTokenList()

--- source/config/validate.ts
+++ source/config/validate.ts
@@ -16,6 +16,35 @@ export function validateIntPosFinite(value:string, id:string) : number {
 	return i;
 }
 
+export function validateStringNonempty(value:string, id:string) : string {
+	if (value === '') throw new Error(`${ id } malformed: empty`);
+
+	return value;
+}
+
+export function validateStringLength(min:number, max:number, value:string, id:string) : string {
+	if (value.length < min) throw new Error(`${ id } malformed: too short`);
+	else if (value.length > max) throw new Error(`${ id } malformed: too long`);
+
+	return value;
+}
+
+export function validateStringHex(value:string, id:string) : string {
+	const low = value.toLowerCase();
+
+	if (!/^[0-9a-f]+$/.test(value)) throw new Error(`${ id } malformed: not a hex string`);
+
+	return low;
+}
+
+export function validateTokenList(value:string, id:string) : string[] {
+	const tokens:string[] = value.split(',').map(val => val.trim());
+
+	if (tokens.some(val => val === '')) throw new Error(`${ id } malformed: not a token list`);
+
+	return tokens;
+}
+
 
 export function processValidationFailure(validation:ValidationFailure, handler:(id:string, err:Error) => void) : void {
 	for (const id of Object.keys(validation.errors)) handler(id, validation.errors[ id ]);
