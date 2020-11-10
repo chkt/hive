d9c7b76e125605d1fdb0c6a139c79a2e91675950
chkt <ck@chkt.one>
2020-11-10 08:53:39 +0100

    Add createSignal()

---
+++ source/common/signal.ts
@@ -0,0 +1,102 @@
+type handle<R> = (handler:Handler) => Promise<R>;
+
+export interface Handler {
+	onSignal(handle:handle<void>) : Handler;
+	numReceived() : number;
+	extend() : Handler;
+	extendWhile<R>(action:handle<R>) : Promise<R>;
+	delegate() : Handler;
+	delegateWhile<R>(action:handle<R>) : Promise<R>;
+	release<R>(value:R) : R;
+}
+
+export interface Signal {
+	send() : Promise<void>;
+	readonly receiver : Handler;
+}
+
+
+const children:WeakMap<Handler, Handler> = new WeakMap();
+const handlers:WeakMap<Handler, handle<void>> = new WeakMap();
+const steps:WeakMap<Handler, number> = new WeakMap();
+const received:WeakMap<Handler, number> = new WeakMap();
+
+const asyncNoop = () => Promise.resolve();
+
+function setHandler(this:Handler, fn:handle<void>) : Handler {
+	handlers.set(this, fn);
+
+	return this;
+}
+
+function setChild(this:Handler) : Handler {
+	const child = createReceiver();
+
+	children.set(this, child);
+
+	return child;
+}
+
+function resetChild<R>(this:Handler, value:R) : R {
+	children.delete(this);
+
+	return value;
+}
+
+function setProxy(this:Handler) : Handler {
+	const child = createReceiver();
+
+	children.set(this, child);
+	steps.set(child, (steps.get(this) ?? 1) + 1);
+
+	return child;
+}
+
+function numReceived(this:Handler) : number {
+	return received.get(this) ?? 0;
+}
+
+function wrap<R>(this:Handler, fn:(abort:Handler) => Promise<R>) : Promise<R> {
+	return fn(this.extend()).then(this.release.bind(this));
+}
+
+function wrap2<R>(this:Handler, fn:(abort:Handler) => Promise<R>) : Promise<R> {
+	return fn(this.delegate()).then(this.release.bind(this));
+}
+
+
+export function createReceiver() : Handler {
+	return {
+		onSignal : setHandler,
+		numReceived,
+		extend : setChild,
+		extendWhile : wrap,
+		delegate : setProxy,
+		delegateWhile : wrap2,
+		release : resetChild
+	};
+}
+
+export function createSignal() : Signal {
+	const receiver = createReceiver();
+
+	return {
+		async send() {
+			const path:Handler[] = [];
+
+			for (let item:Handler | undefined = receiver; item !== undefined; item = children.get(item)) {
+				received.set(item, numReceived.call(item) + 1);
+				path.push(item);
+			}
+
+			for (let i = path.length - 1; i > -1; i -= steps.get(path[i]) ?? 1) {
+				const item = path[i];
+
+				try {
+					await (handlers.get(item) ?? asyncNoop)(item);
+				} catch {}
+			}
+		},
+		receiver
+	}
+}
---
+++ test/common/signal.ts
@@ -0,0 +1,272 @@
+import * as assert from 'assert';
+import { describe, it } from 'mocha';
+
+import { createSignal } from '../../source/common/signal';
+
+
+async function delay(ms:number = 0) : Promise<void> {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms);
+	});
+}
+
+describe('createSignal', () => {
+	it('should send a signal to handler', async () => {
+		const msgs:string[] = [];
+		const signal = createSignal();
+
+		signal.receiver.onSignal(async handler => {
+			msgs.push(`received level 0, ${ handler.numReceived() }`);
+		});
+
+		msgs.push(`sending, ${ signal.receiver.numReceived() }`);
+
+		await signal.send();
+
+		msgs.push(`sent, ${ signal.receiver.numReceived() }`);
+
+		assert.deepStrictEqual(msgs, [
+			'sending, 0',
+			'received level 0, 1',
+			'sent, 1'
+		]);
+	});
+
+	it('should send a signal along handler chain', async () => {
+		const msgs:string[] = [];
+		const signal = createSignal();
+
+		signal.receiver.onSignal(async handler => {
+			msgs.push(`received level 0, ${ handler.numReceived() }`);
+		});
+
+		msgs.push(`level 0, ${ signal.receiver.numReceived() }`);
+
+		new Promise(async (resolve, reject) => {
+			const receiver = signal.receiver.extend().onSignal(async () => {
+				msgs.push(`received level 1, ${ receiver.numReceived() }`);
+				resolve();
+			});
+
+			msgs.push(`level 1, ${ receiver.numReceived() }`);
+
+			await new Promise((resolve2, reject2) => {
+				const receiver2 = receiver.extend().onSignal(async () => {
+					msgs.push(`received level 2, ${ receiver2.numReceived() }`);
+					resolve2();
+				});
+
+				msgs.push(`level 2, ${ receiver.numReceived() }`);
+			});
+
+			msgs.push('resolved p2');
+		})
+			.then(() => msgs.push('resolved p1'));
+
+		msgs.push(`sending, ${ signal.receiver.numReceived() }`);
+
+		await signal.send();
+
+		msgs.push(`sent, ${ signal.receiver.numReceived() }`);
+
+		assert.deepStrictEqual(msgs, [
+			'level 0, 0',
+			'level 1, 0',
+			'level 2, 0',
+			'sending, 0',
+			'received level 2, 1',
+			'resolved p2',
+			'received level 1, 1',
+			'resolved p1',
+			'received level 0, 1',
+			'sent, 1'
+		]);
+	});
+
+	it('should handle ephemeral handler chains', async () => {
+		const msgs:string[] = [];
+		const signal = createSignal();
+
+		signal.receiver.onSignal(async () => {
+			msgs.push(`received level 0, ${ signal.receiver.numReceived() }`);
+		});
+
+		msgs.push(`level 0, ${ signal.receiver.numReceived() }`);
+
+		signal.receiver
+			.extendWhile(receiver => new Promise(resolve => {
+				receiver.onSignal(async () => {
+					msgs.push(`received level 1, ${ receiver.numReceived() }`)
+					resolve('foo');
+				});
+
+				msgs.push(`level 1, ${ receiver.numReceived() }`);
+
+				receiver
+					.extendWhile(async receiver2 => {
+						receiver2.onSignal(async () => {
+							msgs.push(`received level 2, ${ receiver2.numReceived() }`);
+						});
+
+						msgs.push(`level 2, ${ receiver2.numReceived() }`);
+
+						return 'bar';
+					})
+					.then(value => msgs.push(`resolved p2 ${ value }`));
+			}))
+			.then(value => msgs.push(`resolved p1 ${ value }`));
+
+		await delay();
+
+		msgs.push(`sending, ${ signal.receiver.numReceived() }`);
+
+		await signal.send();
+
+		msgs.push(`sent, ${ signal.receiver.numReceived() }`);
+
+		assert.deepStrictEqual(msgs, [
+			'level 0, 0',
+			'level 1, 0',
+			'level 2, 0',
+			'resolved p2 bar',
+			'sending, 0',
+			'received level 1, 1',
+			'received level 0, 1',
+			'resolved p1 foo',
+			'sent, 1'
+		]);
+	});
+
+	it('should handle delegated handler chains', async () => {
+		const msgs:string[] = [];
+		const signal = createSignal();
+
+		signal.receiver.onSignal(async handler => {
+			msgs.push(`received level 0, ${ handler.numReceived() }`);
+		});
+
+		msgs.push(`level 0, ${ signal.receiver.numReceived() }`);
+
+		new Promise(async resolve => {
+			const receiver = signal.receiver.delegate().onSignal(async () => {
+				msgs.push(`received level 1, ${ receiver.numReceived() }`);
+				resolve('baz');
+			});
+
+			msgs.push(`level 1, ${ receiver.numReceived() }`);
+
+			const value2 = await new Promise(resolve2 => {
+				const receiver2 = receiver.delegate().onSignal(async () => {
+					msgs.push(`received level 2, ${ receiver2.numReceived() }`);
+					resolve2('foo');
+				});
+
+				msgs.push(`level 2, ${ receiver.numReceived() }`);
+			});
+
+			msgs.push(`resolved p2 ${ value2 }`);
+
+			resolve('bar');
+		})
+			.then(value => msgs.push(`resolved p1 ${ value }`));
+
+		msgs.push(`sending, ${ signal.receiver.numReceived() }`);
+
+		await signal.send();
+
+		msgs.push(`sent, ${ signal.receiver.numReceived() }`);
+
+		assert.deepStrictEqual(msgs, [
+			'level 0, 0',
+			'level 1, 0',
+			'level 2, 0',
+			'sending, 0',
+			'received level 2, 1',
+			'resolved p2 foo',
+			'resolved p1 bar',
+			'sent, 1'
+		]);
+	});
+
+	it('should handle ephemeral delegated handler chains', async () => {
+		const msgs:string[] = [];
+		const signal = createSignal();
+
+		signal.receiver.onSignal(async () => {
+			msgs.push(`received level 0, ${ signal.receiver.numReceived() }`);
+		});
+
+		msgs.push(`level 0, ${ signal.receiver.numReceived() }`);
+
+		signal.receiver
+			.delegateWhile(receiver => new Promise(resolve => {
+				receiver.onSignal(async () => {
+					msgs.push(`received level 1, ${ receiver.numReceived() }`)
+					resolve('foo');
+				});
+
+				msgs.push(`level 1, ${ receiver.numReceived() }`);
+
+				receiver
+					.delegateWhile(async receiver2 => {
+						receiver2.onSignal(async () => {
+							msgs.push(`received level 2, ${ receiver2.numReceived() }`);
+						});
+
+						msgs.push(`level 2, ${ receiver2.numReceived() }`);
+
+						return 'bar';
+					})
+					.then(value => msgs.push(`resolved p2 ${ value }`));
+			}))
+			.then(value => msgs.push(`resolved p1 ${ value }`));
+
+		await delay();
+
+		msgs.push(`sending, ${ signal.receiver.numReceived() }`);
+
+		await signal.send();
+
+		msgs.push(`sent, ${ signal.receiver.numReceived() }`);
+
+		assert.deepStrictEqual(msgs, [
+			'level 0, 0',
+			'level 1, 0',
+			'level 2, 0',
+			'resolved p2 bar',
+			'sending, 0',
+			'received level 1, 1',
+			'resolved p1 foo',
+			'sent, 1'
+		]);
+	});
+
+	it('should propagate signals irrespective of handling errors', async () => {
+		const msgs:string[] = [];
+		const signal = createSignal();
+
+		signal.receiver
+			.onSignal(async handler => {
+				msgs.push(`received level 0, ${ handler.numReceived() }`);
+			})
+			.extend()
+			.onSignal(handler => {
+				msgs.push(`received level 1, ${ handler.numReceived() }`);
+
+				throw new Error()
+			});
+
+		msgs.push(`sending, ${ signal.receiver.numReceived() }`);
+
+		await signal.send();
+
+		msgs.push(`sent, ${ signal.receiver.numReceived() }`);
+
+		assert.deepStrictEqual(msgs, [
+			'sending, 0',
+			'received level 1, 1',
+			'received level 0, 1',
+			'sent, 1'
+		]);
+	});
+});
