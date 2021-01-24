6bb4e0050561eaa9c17d83fc77656a22d71b4099
chkt <ck@chkt.one>
2021-01-24 01:35:49 +0100

    Add decodeMimeType()

--- source/io/mimeType.ts
+++ source/io/mimeType.ts
@@ -1,10 +1,16 @@
 export type MimeType = [mime_type, mime_encoding];
 
+export interface MimeParts {
+	readonly topType : string;
+	readonly subType : string;
+}
+
 export enum mime_type {
 	bytes = 'application/octet-stream',
 	json = 'application/json',
 	html = 'text/html',
-	text = 'text/plain'
+	text = 'text/plain',
+	sse = 'text/event-stream'
 }
 
 export enum mime_encoding {
@@ -18,4 +24,13 @@ export function isMimeType(str:string) : str is mime_type {
 
 export function isEncoding(str:string) : str is mime_encoding {
 	return Object.values(mime_encoding).includes(str as mime_encoding);
+}
+
+
+export function decodeMimeType(mime:string) : MimeParts {
+	const [ topType, subType ] = mime.toLowerCase().split('/', 2);
+
+	if (topType === '' || subType === undefined || subType === '') throw new Error(`'${ mime }' not a mime type`);
+
+	return { topType, subType };
 }
